a70ab434306892ae6dbe19be9f52e283f3da6aef
Necross08 <135396535+Necross08@users.noreply.github.com>
2023-08-24 16:56:07 -0600

    update
    version 24.8.1

--- js/index.js
+++ js/index.js
@@ -30,3 +30,51 @@ exampleModa2.addEventListener('show.bs.modal', function (event) {
   modalTitle.textContent = 'Hospedajes ' + recipient
   modalBodyInput.value = recipient
 })
+var exampleModa3 = document.getElementById('exampleModa3')
+exampleModa2.addEventListener('show.bs.modal', function (event) {
+  // Botón que activó el modal
+  var button = event.relatedTarget
+  // Extraer información de los atributos data-bs-*
+  var recipient = button.getAttribute('data-bs-whatever')
+  // Si es necesario, puedes iniciar una solicitud AJAX aquí
+  // y luego realiza la actualización en una devolución de llamada.
+  //
+  // Actualizar el contenido del modal.
+  var modalTitle = exampleModa3.querySelector('.modal-title')
+  var modalBodyInput = exampleModa3.querySelector('.modal-body input')
+
+  modalTitle.textContent = 'Tours ' + recipient
+  modalBodyInput.value = recipient
+})
+var exampleModa4 = document.getElementById('exampleModa4')
+exampleModa2.addEventListener('show.bs.modal', function (event) {
+  // Botón que activó el modal
+  var button = event.relatedTarget
+  // Extraer información de los atributos data-bs-*
+  var recipient = button.getAttribute('data-bs-whatever')
+  // Si es necesario, puedes iniciar una solicitud AJAX aquí
+  // y luego realiza la actualización en una devolución de llamada.
+  //
+  // Actualizar el contenido del modal.
+  var modalTitle = exampleModa4.querySelector('.modal-title')
+  var modalBodyInput = exampleModa4.querySelector('.modal-body input')
+
+  modalTitle.textContent = 'Tour Aereo ' + recipient
+  modalBodyInput.value = recipient
+})
+var exampleModa5 = document.getElementById('exampleModa5')
+exampleModa2.addEventListener('show.bs.modal', function (event) {
+  // Botón que activó el modal
+  var button = event.relatedTarget
+  // Extraer información de los atributos data-bs-*
+  var recipient = button.getAttribute('data-bs-whatever')
+  // Si es necesario, puedes iniciar una solicitud AJAX aquí
+  // y luego realiza la actualización en una devolución de llamada.
+  //
+  // Actualizar el contenido del modal.
+  var modalTitle = exampleModa5.querySelector('.modal-title')
+  var modalBodyInput = exampleModa5.querySelector('.modal-body input')
+
+  modalTitle.textContent = 'Canopy Malpais ' + recipient
+  modalBodyInput.value = recipient
+})
